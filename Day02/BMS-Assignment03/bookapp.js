a67316c9078d56a56d3e1511c8ffc7f1d3ae510b
unknown <ravichandrak89@gmail.com>
2020-08-27 23:27:48 +0530

    #Day02-Assignment03

---
+++ Day02/BMS-Assignment03/bookapp.js
@@ -0,0 +1,51 @@
+
+let Book=require('./book'); //Author object is exported
+
+//we will use extraction syntax to extract required fields from module.export
+let { BookManager, showBooks } = require('./bookmanager');
+const { contains, inRange } = require('./utils');
+
+//----- my application logic here ---
+
+let manager = new BookManager();
+let a1 = new Book('book-1','Lost Epic Series',["vivek Mishra"], 150, 4);
+
+let promise = manager.addBook(a1);
+
+promise
+        .then( result=> showBooks(result, "Book details added"))
+        .catch(e=>console.log('error', e));
+
+//manager.addAuthor(a2);
+//manager.addAuthor(a3);
+
+promise = manager.getAllBooks();
+promise
+        .then( result => showBooks(result))
+        .catch(e=>console.log('error', e));
+
+//Search books by Title
+
+promise = manager.search(a => contains(a.title, 'lost'));
+
+promise
+        .then( result=> showBooks(result))
+        .catch(e=>console.log('Error : ', e));
+
+//search books by Price range
+
+promise = manager.search(a => inRange(a.price, 10, 100));
+
+promise
+        .then( result=> showBooks(result))
+        .catch(e=>console.log('Error : ', e));
+
+// Search books by review range
+
+promise = manager.search(a => inRange(a.rating, 1, 3));
+
+promise
+        .then( result=> showBooks(result))
+        .catch(e=>console.log('Error : ', e));
+
+
